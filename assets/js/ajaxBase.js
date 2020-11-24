e29ce18561912284b8386590f795bad16047591e
Nee-Cheung <you@example.com3091643449@qq.com>
2020-11-24 23:09:41 +0800

    后台主页完成

--- assets/js/ajaxBase.js
+++ assets/js/ajaxBase.js
@@ -1,3 +1,16 @@
 $.ajaxPrefilter(function (options) {
-    options.url="http://ajax.frontend.itheima.net"+options.url
+    options.url = "http://ajax.frontend.itheima.net" + options.url
+    if ( options.url.indexOf("/my/")!==-1 ) {
+        options.headers = {
+            Authorization: localStorage.getItem("token")
+        }
+    }
+    //  用户的访问权限
+     options.complete=function (xhr) {
+         console.log( xhr );
+         if (xhr.responseJSON.status===1 && xhr.responseJSON.message==="身份认证失败！") {
+             localStorage.removeItem("token")
+             location.href="login.html"
+         }
+    }
 })
